22c4688d209f53062e08426d393691d5e117c2b8
root <herycahyo90@gmail.com>
2018-04-10 18:50:30 +0700

    tambah model tabel hitung

---
+++ app/Models/Hitung.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hitung extends Model
+{
+    //tabel pada database yang digunakan
+	protected $table = 'hitung';
+
+	//kolom yang tidak boleh di write
+	protected $guarded = ['id'];
+}
